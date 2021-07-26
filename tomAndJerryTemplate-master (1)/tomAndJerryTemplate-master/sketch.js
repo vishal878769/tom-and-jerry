23cb526ef3a5b7d92579613dcc5b176812e0ab74
vishal878769 <77445536+vishal878769@users.noreply.github.com>
2021-07-26 16:08:33 +0530

    Add files via upload

---
+++ tomAndJerryTemplate-master (1)/tomAndJerryTemplate-master/sketch.js
@@ -0,0 +1,63 @@
+var canvas,bg;
+var together;
+var cat, catImg1,catImg2;
+var mouse, mouseImg1,mouseImg2;
+
+function preload() {
+    bg = loadImage("images/garden.png");
+    catImg1= loadAnimation("images/cat1.png");
+    catImg2=loadAnimation("images/cat2.png","images/cat3.png");
+    catImg3= loadAnimation("images/cat4.png");
+    mouseImg1=loadAnimation("images/mouse1.png");
+    mouseImg2= loadAnimation("images/mouse3.png","images/mouse4.png");
+    mouseImg3=loadAnimation("images/mouse1.png");
+}
+
+function setup(){
+    canvas = createCanvas(1000,800);
+
+    mouse = createSprite(200, 600);
+    mouse.addAnimation("mouseStanding", mouseImg1);
+    mouse.scale = 0.15;
+    
+    cat = createSprite(900, 600);
+    cat.addAnimation("catSleeping", catImg1);
+    cat.scale = 0.25;
+
+    catWalk();
+
+}
+
+function draw() {
+
+    background(bg);
+
+    if(cat.x - mouse.x < (cat.width - mouse.width)/2)
+    { 
+        cat.velocityX=0;
+        cat.addAnimation("catLastImage", catImg3);
+    
+        cat.x =300;
+       cat.scale=0.2;
+        cat.changeAnimation("catLastImage");
+
+        mouse.addAnimation("mouseLastImage", mouseImg3);
+        mouse.scale=0.15;
+        mouse.changeAnimation("mouseLastImage");
+    }
+
+
+    drawSprites();
+}
+
+
+function catWalk(){
+
+        mouse.addAnimation("mouseTeasing", mouseImg2)
+      mouse.changeAnimation("mouseTeasing");
+        mouse.frameDelay = 25; 
+
+      cat.velocityX = -5; 
+    cat.addAnimation("catRunning", catImg2);
+        cat.changeAnimation("catRunning");
+}
